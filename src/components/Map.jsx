b0e7d4a6ff53638ee3ad2d19f4e3517788dc71e0
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2025-09-10 21:08:10 +0700

    feat: read the URL and then render it to the component

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,8 +1,20 @@
 import React from "react";
 import styles from "./Map.module.css";
+import { useSearchParams } from "react-router";
 
 function Map() {
-  return <div className={styles.mapContainer}>Map</div>;
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+
+  return (
+    <div className={styles.mapContainer}>
+      <h1>Map</h1>
+      <p>Lat: {lat}</p>
+      <p>Lng: {lng}</p>
+    </div>
+  );
 }
 
 export default Map;
