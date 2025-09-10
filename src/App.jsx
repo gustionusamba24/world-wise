5b9f1bf692d48cfce21dc23103bef5b4c4dad5e5
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2025-09-10 21:57:35 +0700

    feat: programmatic navigation with <Navigation />

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { BrowserRouter, Route, Routes } from "react-router";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router";
 
 import Homepage from "./pages/Homepage";
 import Product from "./pages/Product";
@@ -44,10 +44,7 @@ function App() {
         <Route path="/pricing" element={<Pricing />} />
         <Route path="/login" element={<Login />} />
         <Route path="/app" element={<AppLayout />}>
-          <Route
-            index
-            element={<CityList cities={cities} isLoading={isLoading} />}
-          />
+          <Route index element={<Navigate replace to="cities" />} />
           <Route
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
